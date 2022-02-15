2c92064eac901b363845fbbe1646b7b03de216b5
Eugene Korobenkov <joe_kor@mail.ru>
2022-02-15 22:28:08 +0800

    14 css-modul

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+        <img src= 'https://image.shutterstock.com/image-vector/vector-illustration-icon-bunch-ripe-260nw-646958365.jpg'/>
+        
+    </header>
+}
+
+export default Header;
