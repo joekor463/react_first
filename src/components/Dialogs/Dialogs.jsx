ed3c0824778157eb80341edc72709cf5969a1a37
Eugene Korobenkov <joe_kor@mail.ru>
2022-03-02 22:53:15 +0800

    21 modify dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,32 @@ import s from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <div>
-            Dialogs
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog}>
+                        Joe
+                    </div>
+                    <div className={s.dialog + ' ' + s.active }>
+                        An
+                    </div>
+                    <div className={s.dialog}>
+                       Sofi
+                    </div>
+                    <div className={s.dialog}>
+                        Kristy
+                    </div>
+                    <div className={s.dialog}>
+                        Unknow
+                    </div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hello</div>
+                    <div className={s.message}>What's up?</div>
+                    <div className={s.message}>Where are you?</div>
+                    <div className={s.message}>What's going on?</div>
+                    <div className={s.message} >We in submarine</div>
+                </div>
+            </div>
         </div>
     )
 }
