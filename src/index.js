e0f0549ecbeab23f8b41eabf65aadd74c0e404c3
Eugene Korobenkov <joe_kor@mail.ru>
2022-03-27 07:59:57 +0800

    35 add observer subscribe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import reportWebVitals from './reportWebVitals';
-import state, {addPost} from "./redux/state";
-import  {rerenderEntireTree} from './render';
-import ReactDOM from "react-dom";
 import App from "./App";
+import ReactDOM from 'react-dom';
+import './index.css';
+import state, {addPost, subscribe, updateNewPostText} from "./redux/state";
 
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>,
+        document.getElementById('root'));
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
