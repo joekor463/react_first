59375013b0c49612e68058605e8faceb279a2099
Joe <joe_kor@mail.ru>
2022-06-06 08:11:10 +0300

    61 add login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import React from 'react';
-import Header from "./components/Header/Header";
+
 import Navbar from "./components/Navbar/Navbar";
 import DialogsContainer  from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import {Route, Routes } from "react-router-dom";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 
 const App = () => {
     return  (
             <div className='app-wrapper'>
-                <Header/>
+                <HeaderContainer />
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img src='https://www.nicepng.com/png/detail/976-9761880_v-for-vendetta-clipart-anonymous-v-for-vendetta.png'/>
+            <div className={s.loginBlock}>
+                { props.isAuth ? props.login
+                : <NavLink to={'/login'} >Login</NavLink>}
+            </div>
         </header>)
-
 }
 
 export default Header;
